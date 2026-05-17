04bf8d06380dc34f1d3521aff4a88920c6e50dfa
Abushaker Jamil <abushakerjamil254@mail.com>
2026-05-17 23:10:57 +0600

    first commit

---
+++ src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGOURI);
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+}
+
+module.exports = connectDB;
