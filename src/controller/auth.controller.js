2d6406c9ea75ca136f8c3661a274a3335b6fcd84
Abushaker Jamil <abushakerjamil254@mail.com>
2026-05-19 22:21:36 +0600

    Updated controller models

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -4,6 +4,16 @@ const jwt = require("jsonwebtoken");
 async function registerUser(req, res) {
   const { username, email, password } = req.body;
 
+  const isUserAlreadyExist = await userModel.findOne({
+    email,
+  });
+
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      msg: "User Already exist ",
+    });
+  }
+
   const user = await userModel.create({
     username,
     email,
@@ -16,6 +26,22 @@ async function registerUser(req, res) {
     },
     process.env.JWT_SECRET,
   );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    msg: "User registered Successfully",
+    user,
+    token,
+  });
 }
 
-module.exports = { registerUser };
+async function getRegister(req, res) {
+  console.log("Cookies", req.cookies);
+  res.json({
+    msg: "Test route",
+    cookies: req.cookies,
+  });
+}
+
+module.exports = { registerUser, getRegister };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require("../controller/auth.controller");
 const route = express.Router();
 
 route.post("/register", authController.registerUser);
+route.get("/test", authController.getRegister);
 
 module.exports = route;
